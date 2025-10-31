3253a8205bb1f504f2328ddba1e704d978a03374
Sanyam Jaiswal <sjaisw12@asu.edu>
2025-10-31 10:04:38 -0700

    Initial Commit

---
+++ CommunityEventExplorer/CommunityEventExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityEventExplorerApp.swift
+//  CommunityEventExplorer
+//
+//  Created by Sanyam Jaiswal on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CommunityEventExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
